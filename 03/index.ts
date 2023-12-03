b15927e930fca256a536215d5ef721f047e93229
eszczepan <s.szablej1@gmail.com>
2023-12-03 13:13:44 +0100

    03: refactor first part

--- 03/index.ts
+++ 03/index.ts
@@ -4,6 +4,12 @@ const text = await file.text();
 const puzzleArray = text.split('\n');
 const isDigitRegex = /^\d+$/;
 
+interface Elements {
+  previousElement: string;
+  elementsToCheck: string[];
+  nextElement: string;
+}
+
 function isDigit(str: string): boolean {
   return isDigitRegex.test(str);
 }
@@ -19,15 +25,47 @@ function checkIsEnginePart(elementsToCheck: string[]): boolean {
   return isEnginePart;
 }
 
+function getElements(
+  elementIndex: number,
+  rowIndex: number,
+  currentRow: string
+): Elements {
+  const previousRow = puzzleArray[rowIndex - 1] || '';
+  const nextRow = puzzleArray[rowIndex + 1] || '';
+
+  const previousElement = currentRow[elementIndex - 1] || '';
+  const nextElement = currentRow[elementIndex + 1] || '';
+  const topLeftElement = previousRow[elementIndex - 1] || '';
+  const topElement = previousRow[elementIndex] || '';
+  const topRightElement = previousRow[elementIndex + 1] || '';
+  const bottomLeftElement = nextRow[elementIndex - 1] || '';
+  const bottomElement = nextRow[elementIndex] || '';
+  const bottomRightElement = nextRow[elementIndex + 1] || '';
+  const elementsToCheck = [
+    previousElement,
+    nextElement,
+    topLeftElement,
+    topElement,
+    topRightElement,
+    bottomLeftElement,
+    bottomElement,
+    bottomRightElement,
+  ];
+
+  return {
+    previousElement,
+    elementsToCheck,
+    nextElement,
+  };
+}
+
 function getEnginePartsSum(): number {
   let result = 0;
   let currentNumber = '';
   let isEnginePart = false;
 
   for (let i = 0; i < puzzleArray.length; i++) {
-    const previousRow = puzzleArray[i - 1] || '';
     const currentRow = puzzleArray[i];
-    const nextRow = puzzleArray[i + 1] || '';
 
     for (let j = 0; j < currentRow.length; j++) {
       const currentElement = currentRow[j];
@@ -36,24 +74,8 @@ function getEnginePartsSum(): number {
         continue;
       }
 
-      const previousElement = currentRow[j - 1] || '';
-      const nextElement = currentRow[j + 1] || '';
-      const topLeftElement = previousRow[j - 1] || '';
-      const topElement = previousRow[j] || '';
-      const topRightElement = previousRow[j + 1] || '';
-      const bottomLeftElement = nextRow[j - 1] || '';
-      const bottomElement = nextRow[j] || '';
-      const bottomRightElement = nextRow[j + 1] || '';
-      const elementsToCheck = [
-        previousElement,
-        nextElement,
-        topLeftElement,
-        topElement,
-        topRightElement,
-        bottomLeftElement,
-        bottomElement,
-        bottomRightElement,
-      ];
+      const elements = getElements(j, i, currentRow);
+      const { previousElement, elementsToCheck, nextElement } = elements;
 
       // FIRST ELEMENT OF ROW - clear state
       if (previousElement === undefined) {
