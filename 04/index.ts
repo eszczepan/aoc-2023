7b28964c24a244f7b83497c7d797d5d7f59f59c8
eszczepan <s.szablej1@gmail.com>
2023-12-04 23:06:42 +0100

    04: first second part

--- 04/index.ts
+++ 04/index.ts
@@ -3,18 +3,28 @@ const file = Bun.file(path);
 const text = await file.text();
 const puzzleArray = text.split('\n');
 
-function getScratchcardsPoints(): number {
-  const scratchcardsTable = puzzleArray.map((line) => {
+function getScratchcardsTable(): Array<Array<string>> {
+  return puzzleArray.map((line) => {
     const indexOfColon = line.indexOf(':');
     const convertedScratchcards = line.slice(indexOfColon + 2, line.length).split(' | ');
 
     return convertedScratchcards;
   });
+}
+
+function getCards(scratchcards: string[]) {
+  const winningCard = scratchcards[0].split(' ');
+  const scratchCard = scratchcards[1].split(' ');
+
+  return { winningCard, scratchCard };
+}
+
+function getScratchcardsPoints(): number {
+  const scratchcardsTable = getScratchcardsTable();
 
   const score = scratchcardsTable.reduce((acc, scratchcards) => {
+    const { winningCard, scratchCard } = getCards(scratchcards);
     let scoreCard = 0;
-    const winningCard = scratchcards[0].split(' ');
-    const scratchCard = scratchcards[1].split(' ');
 
     winningCard.forEach((card) => {
       if (card && scratchCard.includes(card)) {
@@ -28,6 +38,35 @@ function getScratchcardsPoints(): number {
   return score;
 }
 
-const scratchcardsPoints = getScratchcardsPoints();
+function getScratchcardsNumber(): number {
+  const scratchcardsTable = getScratchcardsTable();
+  const scratchCardsCopies = scratchcardsTable.reduce(
+    (cards: Record<string, number>, scratchcards: string[], index: number) => {
+      const { winningCard, scratchCard } = getCards(scratchcards);
+      const cardIndex = index + 1;
+      let winningCardCount = 1;
+      cards[cardIndex] = cards[cardIndex] + 1 || 1;
 
-console.log({ scratchcardsPoints });
+      winningCard.forEach((card) => {
+        if (card && scratchCard.includes(card)) {
+          const cardToIncrement = cardIndex + winningCardCount;
+          for (let i = 0; i < cards[cardIndex]; i++) {
+            cards[cardToIncrement] = cards[cardToIncrement] + 1 || 1;
+          }
+          winningCardCount += 1;
+        }
+      });
+
+      return cards;
+    },
+    {}
+  );
+
+  const scratchcardsNumber = Object.values(scratchCardsCopies).reduce((a: number, b: number) => a + b, 0);
+
+  return scratchcardsNumber;
+}
+
+const scratchcardsPoints = getScratchcardsPoints();
+const scratchcardsNumber = getScratchcardsNumber();
+console.log({ scratchcardsPoints, scratchcardsNumber });
