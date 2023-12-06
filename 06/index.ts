8c57ac96aa6629ea450d4c251e831f01a5bbe195
eszczepan <s.szablej1@gmail.com>
2023-12-06 16:16:22 +0100

    06: second part

--- 06/index.ts
+++ 06/index.ts
@@ -2,13 +2,20 @@ import { getInput } from '../utils';
 
 const input = await getInput('06/input.txt');
 
-function parseInput(input: string[]) {
+function parseInputToMultiple(input: string[]): Array<Array<number>> {
   const convertedInput = input.map((s) => s.split(': ')[1].split(' ').filter(Boolean).map(Number));
   return convertedInput[0].map((_, i) => convertedInput.map((row) => row[i]));
 }
 
-function getWinOptions(input: string[]) {
-  const races = parseInput(input);
+function parseInputToOneRace(input: string[]): number[] {
+  return input.map((input) => {
+    const words = input.trim().replace(/\s+/g, ' ').split(' ').slice(1);
+    return Number(words.join(''));
+  });
+}
+
+function getWinOptions(input: string[]): number {
+  const races = parseInputToMultiple(input);
   let totalWinOptions = 1;
 
   races.forEach(([time, record]) => {
@@ -28,6 +35,23 @@ function getWinOptions(input: string[]) {
   return totalWinOptions;
 }
 
-const winOptions = getWinOptions(input);
+function getLongRaceWinOptions(input: string[]): number {
+  let winOptions = 0;
+  const [time, record] = parseInputToOneRace(input);
 
-console.log({ winOptions });
+  for (let i = 1; i < time; i++) {
+    const speed = i;
+    const duration = time - i;
+    const distanceCovered = speed * duration;
+
+    if (distanceCovered > record) {
+      winOptions++;
+    }
+  }
+
+  return winOptions;
+}
+
+const winOptions = getWinOptions(input);
+const winOpitionsLongRace = getLongRaceWinOptions(input);
+console.log({ winOptions, winOpitionsLongRace });
