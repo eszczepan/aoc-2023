658934da0c9b2e9ee066e07bbe158f608227d8be
eszczepan <s.szablej1@gmail.com>
2023-12-14 15:39:17 +0100

    08: naming

--- 08/index.ts
+++ 08/index.ts
@@ -4,37 +4,37 @@ const input = await getInput('08/input.txt');
 
 function parseInput(input: string[]) {
   const sequence = input[0];
-  const elements = input.slice(2);
+  const nodes = input.slice(2);
 
-  return { sequence, elements };
+  return { sequence, nodes };
 }
 
-function createElementsObject(elements: string[]) {
-  const elementsObject: Record<string, Array<string>> = {};
-  elements.forEach((element) => {
-    const [el, L, R] = element
+function createnodesObject(nodes: string[]) {
+  const nodesObject: Record<string, Array<string>> = {};
+  nodes.forEach((node) => {
+    const [el, L, R] = node
       .replace(/[=() ,]+/g, ' ')
       .trim()
       .split(' ');
 
-    elementsObject[el] = [L, R];
+    nodesObject[el] = [L, R];
   });
 
-  return elementsObject;
+  return nodesObject;
 }
 
 function getStepsToZZZ(input: string[]): number {
-  const { sequence, elements } = parseInput(input);
-  const elementsObject = createElementsObject(elements);
-  let currentElement = 'AAA';
+  const { sequence, nodes } = parseInput(input);
+  const nodesObject = createnodesObject(nodes);
+  let currentNode = 'AAA';
   let steps = 0;
 
-  while (currentElement !== 'ZZZ') {
+  while (currentNode !== 'ZZZ') {
     for (let i = 0; i < sequence.length; i++) {
       const direction = sequence[i] === 'L' ? 0 : 1;
-      currentElement = direction ? elementsObject[currentElement][1] : elementsObject[currentElement][0];
+      currentNode = direction ? nodesObject[currentNode][1] : nodesObject[currentNode][0];
       steps++;
-      if (currentElement === 'ZZZ') break;
+      if (currentNode === 'ZZZ') break;
     }
   }
 
